086f3163ab09214916a41a3351244d08b6b7990c
Berigny <134203992+Berigny@users.noreply.github.com>
2023-09-12 21:51:52 +1000

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 import os
 import openai
 import streamlit as st
+import base64
+import io
 
+# Set the OpenAI API key
 openai.api_key = os.environ.get("OPENAI_API_KEY")
 
 # Load the HTML file
@@ -11,7 +14,22 @@ with open('audio_recorder.html', 'r') as file:
 # Embed the HTML in the Streamlit app
 st.components.v1.html(html, height=600)
 
+# A hypothetical function to process the audio data with OpenAI
+def process_audio_with_openai(audio_data):
+    # Here, you would include the code to process the audio data with OpenAI
+    # For now, it's just a placeholder function
+    pass
+
 # Check if audio data has been received
 if "audio_data" in st.session_state:
-    # Your processing code here (e.g., send the audio data to OpenAI's API)
-    st.audio(st.session_state.audio_data, format='audio/wav')
+    # Decode the base64 string to get the audio data as a bytes object
+    audio_data = base64.b64decode(st.session_state.audio_data)
+    
+    # Create a BytesIO object from the audio data
+    audio_bytes_io = io.BytesIO(audio_data)
+    
+    # Display the audio player with the audio data
+    st.audio(audio_bytes_io, format='audio/wav')
+
+    # Process the audio data with OpenAI
+    process_audio_with_openai(audio_data)
